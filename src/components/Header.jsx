61aeacb3fbb205febbd759a33b9156062a99bc34
BrunoSouza <desouza88@hotmail.com>
2023-01-14 11:59:46 -0300

    04 - Requisito 04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,6 +33,19 @@ class Header extends React.Component {
 
     return (
       <header data-testid="header-component">
+        <ul>
+          <li>
+            <Link to="/search" data-testid="link-to-search">
+              Procura
+            </Link>
+            <Link to="/favorites" data-testid="link-to-favorites">
+              Músicas Favoritas
+            </Link>
+            <Link to="/profile" data-testid="link-to-profile">
+              Perfil
+            </Link>
+          </li>
+        </ul>
         <p data-testid="header-user-name">
           { userName }
         </p>
